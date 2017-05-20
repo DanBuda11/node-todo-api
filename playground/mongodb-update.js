1fbf0e811207e7bf906100d5182ccea2245be6ba
Dan Buda <danbuda@gmail.com>
2017-05-19 19:57:20 -0500

    add update scripts

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server.');
+	}
+	console.log('Connected to MongoDB server.');
+
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('591f8dd5271eb13065da9311')
+	// }, {
+	// 	$set: {
+	// 		completed: true
+	// 	}
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('591f8fe6271eb13065da949d')
+	}, {
+		$set: {
+			name: 'Dan'
+		},
+		$inc: {
+			age: 1
+		}
+	}, {
+		returnOriginal: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+
+	// db.close();
+});
