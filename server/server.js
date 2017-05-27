b8fbb390eff76193f31cd25067a93d994c81b2ce
Dan Buda <danbuda@gmail.com>
2017-05-27 16:52:04 -0500

    update user model and start auth

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { User } = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
@@ -94,6 +95,17 @@ app.patch('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send();
 	})
+});
+
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then((user) => {
+		res.send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})	
 });
 
 app.listen(port, () => {
