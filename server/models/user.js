9893d7a5452088824603943d0a652f44d7fe4093
Dan Buda <danbuda@gmail.com>
2017-05-28 13:26:53 -0500

    add auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,24 @@ UserSchema.methods.generateAuthToken = function() {
 	});
 };
 
+// Statics makes it a model method instead of an instance method
+UserSchema.statics.findByToken = function(token) {
+	var User = this;
+	var decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		_id: decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	});
+};
+
 // Create mongoose User model
 var User = mongoose.model('User', UserSchema);
 
