2bfad40d9d25c6ba862932ff3105869ce9abb9b1
Dan Buda <danbuda@gmail.com>
2017-05-30 01:10:37 -0500

    add tests for delete route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -286,11 +286,30 @@ describe('POST /users/login', () => {
 				expect(res.headers['x-auth']).toNotExist();
 			})
 			.end((err, res) => {
-				if(err) {
+				if (err) {
 					return done(err);
 				}
 
 				User.findById(users[1]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+});
+
+describe('DELETE /users/me/token', () => {
+	it('should remove auth token on logout', (done) => {
+		request(app)
+			.delete('/users/me/token')
+			.set('x-auth', users[0].tokens[0].token)
+			.expect(200)
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				User.findById(users[0]._id).then((user) => {
 					expect(user.tokens.length).toBe(0);
 					done();
 				}).catch((e) => done(e));
